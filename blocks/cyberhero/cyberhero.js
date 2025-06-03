57a8258569cb0e9182d8171c0d1fd953be595ad4
Jurica Migač <jurica.migac@cyber64.com>
2025-06-03 08:15:41 +0200

    Custom Hero + Sidekick configurations (#1)
    * Custom Hero + Sidekick configurations
    
    * Reformating .js file
    
    * Formatting
    
    * Adding argument into parentheses

---
+++ blocks/cyberhero/cyberhero.js
@@ -0,0 +1,15 @@
+export default async function decorate(block) {
+  const rootElement = document.createElement('div');
+  rootElement.classList.add('cyberhero');
+  const picture = block.querySelector('picture');
+  const paragraphs = block.querySelectorAll('p');
+
+  rootElement.append(picture);
+  paragraphs.forEach((element) => {
+    if (element.value !== '') {
+      rootElement.append(element);
+    }
+  });
+  block.innerHTML = '';
+  block.append(rootElement);
+}
