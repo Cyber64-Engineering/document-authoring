1016af260973f195ba5cedc3f1279f27f6f643c7
Jurica Migač <jurica.migac@cyber64.com>
2025-06-06 12:58:35 +0200

    Calculator Host update (#9)
    * Calculator Host update
    
    * Using variable defined hostname

--- blocks/creditcalculator/creditcalculator.js
+++ blocks/creditcalculator/creditcalculator.js
@@ -1,5 +1,3 @@
-import calculatorInfo from "./mockResponse.json" with { type: "json" };
-
 function formatRSD(value) {
   return value.toLocaleString('sr-RS', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
 }
@@ -50,7 +48,6 @@ function updateCalculator(calculatorInfo) {
 }
 
 export default async function decorate(block) {
-
   const rootElement = document.createElement('div');
   rootElement.classList.add('creditcalculator');
 
@@ -160,6 +157,20 @@ export default async function decorate(block) {
   block.append(rootElement);
 
   /* API CALL */
+  const apiHostName = `${window.location.protocol}//${window.location.host}`;
+  const calculatorData = await fetch(
+    `${apiHostName}/financial-site/data/credit.json`,
+    {
+      headers: {
+        accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+      },
+      body: null,
+      method: 'GET',
+      mode: 'cors',
+      credentials: 'omit',
+    },
+  );
+  const calculatorInfo = await calculatorData.json();
 
   updateCalculator(calculatorInfo);
 
@@ -169,5 +180,4 @@ export default async function decorate(block) {
   inputRangeLoanPeriodAmount.addEventListener('input', () => {
     updateCalculator(calculatorInfo);
   });
-  
 }
