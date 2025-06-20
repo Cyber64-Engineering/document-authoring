1dcf3f5211548e0587e0005e953e851567f11b66
Danijela Briški <danijela.briski@cyber64.com>
2025-06-20 15:15:23 +0200

    Add compile jsx bash script to compile all files from each block, create text preact example

---
+++ blocks/text-preact/components/textComponent.js
@@ -0,0 +1,6 @@
+import { h } from 'https://esm.sh/preact';
+
+const TextComponent = () => h('h1', {
+  class: 'flex-1',
+}, 'Text component Preact');
+export default TextComponent;
---
+++ blocks/text-preact/jsx-components/textComponent.jsx
@@ -0,0 +1,4 @@
+const TextComponent = () => {
+  return <h1 className="flex-1">Text component Preact</h1>;
+};
+export default TextComponent;
