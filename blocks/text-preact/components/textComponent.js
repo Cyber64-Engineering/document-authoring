facb8ca87acf60ceb7e9443d10b6538da9ef4fb6
Danijela Briški <danijela.briski@cyber64.com>
2025-06-20 16:28:56 +0200

    Remove text preact component

--- blocks/text-preact/components/textComponent.js
+++
@@ -1,6 +0,0 @@
-import { h } from 'https://esm.sh/preact';
-
-const TextComponent = () => h('h1', {
-  class: 'flex-1',
-}, 'Text component Preact');
-export default TextComponent;
--- blocks/text-preact/jsx-components/textComponent.jsx
+++
@@ -1,4 +0,0 @@
-const TextComponent = () => {
-  return <h1 className="flex-1">Text component Preact</h1>;
-};
-export default TextComponent;
