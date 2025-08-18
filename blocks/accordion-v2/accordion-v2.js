e27f11523667e7fbc411328c27870a662be0aad2
Danijela Briški <danijela.briski@cyber64.com>
2025-08-18 15:01:51 +0200

    first draft of accordion v2 element

--- blocks/accordion-v2/accordion-v2.js
+++ blocks/accordion-v2/accordion-v2.js
@@ -1,3 +1,32 @@
 export default function decorate(block) {
-  console.log(block);
+  [...block.children].forEach((row) => {
+    const label = row.children[0].children[0];
+    const summary = document.createElement('summary');
+    summary.className = 'accordion-item-label';
+    summary.append(label);
+    const body = row.children[0].children[0];
+    const itemContainer = document.createElement('div');
+    itemContainer.className = 'accordion-item-body';
+    itemContainer.append(...body.childNodes);
+    [...itemContainer.children].forEach((innerRow) => {
+      console.log(innerRow);
+      const labelInner = innerRow.children[0].children[0];
+      const summaryInner = document.createElement('summary');
+      const titleInner = document.createElement('p');
+      titleInner.append(...labelInner.childNodes);
+      summaryInner.className = 'accordion-item-label';
+      summaryInner.append(titleInner);
+      const bodyInner = innerRow.children[0].children[0];
+      const itemContainerInner = document.createElement('div');
+      itemContainerInner.append(...bodyInner.childNodes);
+      const detailsInner = document.createElement('details');
+      detailsInner.className = 'accordion-item';
+      detailsInner.append(summaryInner, itemContainerInner);
+      innerRow.replaceWith(detailsInner);
+    });
+    const details = document.createElement('details');
+    details.className = 'accordion-item';
+    details.append(summary, itemContainer);
+    row.replaceWith(details);
+  });
 }
